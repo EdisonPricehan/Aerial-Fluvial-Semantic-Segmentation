5b6b3d9a2d7e8a50a43aa38ae3000c2cf6ac5063
Edison-Windows <princehan920453024@gmail.com>
2022-06-25 03:59:15 +0800

    move all class rgb arrays to constants.py

--- src/utils/convert_images.py
+++ src/utils/convert_images.py
@@ -8,16 +8,7 @@ import fire
 from skimage import io
 from tqdm import tqdm
 
-
-water_rgb_aerial = [128, 64, 128]
-bridge_rgb = [119, 11, 32]
-dry_sediment_rgb = [220, 220, 0]
-vegetation_rgb = [107, 142, 35]
-wood_in_river_rgb = [150, 120, 90]
-self_rgb = [10, 0, 0]
-sky_rgb = [70, 130, 180]
-boat_rgb = [0, 0, 230]
-water_rgb_boat = [255, 255, 255]
+from constants import *
 
 
 def get_filelist(dir, Filelist):
