e7f1181e0aac90adbb76ad05021fca4bb34d2ab1
Edison-Windows <princehan920453024@gmail.com>
2022-07-11 17:02:10 -0400

    add function to add new config to existing wandb run.

--- src/utils/wandb_utils.py
+++ src/utils/wandb_utils.py
@@ -13,9 +13,24 @@ def update_config(run, key, new_conf):
         print(f"{key} is not in the config keys.")
 
 
+def add_config(run, key, new_conf):
+    if key in run.config.keys():
+        print(f"{key} is already in the config keys.")
+    else:
+        run.config[key] = new_conf
+        run.update()
+        print(f"Added {key} with {new_conf}.")
+
+
 if __name__ == '__main__':
     api = wandb.Api()
     # specify the run to update
-    run = api.run("edisonprincehan/baseline_test/3l40t08l")
+    run = api.run("edisonprincehan/baseline_test/qtnzqus4")
 
-    update_config(run, "trainset", "pretraining")
+    # update existing config
+    # update_config(run, "trainset", "pretraining")
+
+    # add new config
+    # add_config(run, "Params Mb", 32.5)
+    # add_config(run, "GFLOPs", 28.35)
+
