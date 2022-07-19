02b0b6ff0fad03ab28fa64899eb1d7a5c907f226
Edison-Windows <princehan920453024@gmail.com>
2022-07-19 11:18:55 -0400

    use test set path as inference class argument; add trainset and testset name as arguments when initing wandb logger.

--- src/networks/inference.py
+++ src/networks/inference.py
@@ -14,8 +14,8 @@ from SS_Model_Lit import SSModelGeneric as ssmg, check_encoder_existence, check_
 def inference(model, test_dataloader, logger=None):
     trainer = pl.Trainer(gpus=1, max_epochs=1, logger=(logger if logger is not None else False))
     # start testing
-    # trainer.test(model, test_dataloader)
-    return trainer.predict(model, dataloaders=test_dataloader)
+    trainer.test(model, test_dataloader, verbose=True)
+    # return trainer.predict(model, dataloaders=test_dataloader)
 
 
 def get_model(model_path):
@@ -25,32 +25,36 @@ def get_model(model_path):
     return m
 
 
-def get_dataloader(ds_dir):
-    dataset_dir = os.path.join(os.path.dirname(__file__), ds_dir)
-    test_data = FluvialDataset(dataset_dir, train=False, use_augment=False, transform=resize, target_transform=resize)
+def get_dataloader(ds_path):
+    test_data = FluvialDataset(ds_path, use_augment=False, transform=resize, target_transform=resize)
     print(f"Test num: {len(test_data)}")
     print(f"Image size: {test_data[0][0].shape}, mask size: {test_data[0][1].shape}")
     test_dataloader = DataLoader(test_data, batch_size=test_bs, shuffle=False)
     return test_dataloader
 
 
-def get_logger(ds_dir='', encoder_name='', decoder_name='', test_batch_size=None):
-    if ds_dir == '' or encoder_name == '' or decoder_name == '' or test_batch_size is None:
+def get_logger(encoder_name='', decoder_name='', project_name='', test_batch_size=None,
+               trainset='pretraining', testset='wabash_wildcat'):
+    if encoder_name == '' or decoder_name == '' or project_name == '' or test_batch_size is None:
         print("Need arguments to init logger, return None logger.")
         return None
 
-    wandb_logger = WandbLogger(project=os.path.basename(ds_dir), name='-'.join([encoder_name, decoder_name, 'test']),
+    wandb_logger = WandbLogger(project=project_name,
+                               name='-'.join([encoder_name, decoder_name, trainset, testset, 'test']),
                                log_model=False, anonymous=False,
                                save_dir=os.path.join(os.path.dirname(__file__), '../logs'))
-    wandb_logger.experiment.config.update({'test_bs': test_batch_size})
+    wandb_logger.experiment.config.update({'test_bs': test_batch_size,
+                                           'trainset': trainset,
+                                           'testset': testset})
     return wandb_logger
 
 
 if __name__ == '__main__':
     # parse necessary arguments if run as script
     parser = argparse.ArgumentParser(description="Pytorch Lightning Testing")
-    parser.add_argument('dataset_dir', metavar='DIR', help='Relative path to dataset')
+    parser.add_argument('dataset_path', type=str, help='Relative path to dataset')
     parser.add_argument('model_path', type=str, default='', help='Relative path of model to be loaded')
+    parser.add_argument('-p', '--project-name', type=str, default='', help='Project name')
     parser.add_argument('-tb', '--test-batch-size', type=int, default=1, metavar='N',
                         help='Test batch size override (default: 1)')
     parser.add_argument('-e', '--encoder', type=str, default='resnet34', help='Encoder type')
@@ -62,12 +66,13 @@ if __name__ == '__main__':
     pprint(args.__dict__)
 
     # parse arguments as local variables
-    dataset_dir = args.dataset_dir
+    dataset_path = args.dataset_path
     model_path = args.model_path
     test_bs = args.test_batch_size
     encoder = args.encoder
     decoder = args.decoder
     out_class_num = args.out_class_num
+    project_name = args.project_name
 
     # check encoder existence
     if not check_encoder_existence(encoder):
@@ -78,10 +83,11 @@ if __name__ == '__main__':
         exit(0)
 
     # init dataloader
-    test_dataloader = get_dataloader(dataset_dir)
+    test_dataloader = get_dataloader(dataset_path)
 
     # init logger
-    logger = get_logger(ds_dir=dataset_dir, encoder_name=encoder, decoder_name=decoder, test_batch_size=test_bs)
+    logger = get_logger(encoder_name=encoder, decoder_name=decoder, project_name=project_name, test_batch_size=test_bs,
+                        trainset='pretraining_wabash_wildcat', testset='wabash_wildcat')
 
     # construct desired model if you are sure your checkpoint has no hyper-parameters
     # model = ssmg(arch=decoder, encoder_name=encoder, in_channels=3, out_classes=out_class_num)
