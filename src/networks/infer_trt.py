d60088fc62d3e6123d88853cf8616cb43d6e25cb
Edison-JetsonOrinNano <princehan920453024@gmail.com>
2025-05-01 18:03:04 -0400

    decouple preprocessing, infer and postprocessing code for trt inference.

--- src/networks/infer_trt.py
+++ src/networks/infer_trt.py
@@ -8,7 +8,7 @@ import torch
 import os
 import time
 import numpy as np
-from typing import Optional, Tuple
+from typing import Optional, Tuple, List
 from PIL import Image
 
 try:
@@ -23,6 +23,15 @@ try:
     from cuda import cuda
 except ImportError as e:
     raise ImportError(f'Please install cuda using "pip install cuda-python", {e}.')
+
+
+__all__ = ['build_engine',
+           'trt_inference',
+           'infer',
+           'load_and_process_img',
+           'post_process_mask',
+           'get_engine_context',
+           'validate_onnx']
 
 
 # Image shape is hard coded here and needs to align with the trained segmentation model
@@ -109,13 +118,14 @@ def trt_inference(engine, context, data):
     """
     nInput = np.sum([engine.binding_is_input(i) for i in range(engine.num_bindings)])
     nOutput = engine.num_bindings - nInput
-    print('nInput:', nInput)
-    print('nOutput:', nOutput)
 
-    for i in range(nInput):
-        print("Bind[%2d]:i[%2d]->" % (i, i), engine.get_binding_dtype(i), engine.get_binding_dtype(i))
-    for i in range(nInput, nInput + nOutput):
-        print("Bind[%2d]:o[%2d]->" % (i, i - nInput), engine.get_binding_dtype(i), engine.get_binding_dtype(i))
+    # For debugging
+    # print('nInput:', nInput)
+    # print('nOutput:', nOutput)
+    # for i in range(nInput):
+    #     print("Bind[%2d]:i[%2d]->" % (i, i), engine.get_binding_dtype(i), engine.get_binding_dtype(i))
+    # for i in range(nInput, nInput + nOutput):
+    #     print("Bind[%2d]:o[%2d]->" % (i, i - nInput), engine.get_binding_dtype(i), engine.get_binding_dtype(i))
 
     bufferH = []
     bufferH.append(np.ascontiguousarray(data.reshape(-1)))
@@ -141,10 +151,57 @@ def trt_inference(engine, context, data):
     return bufferH
 
 
-def infer(engine_path: str, img_path: str, mask_path: Optional[str] = None) -> Tuple[np.ndarray, np.ndarray]:
+def load_and_process_img(img_path: str, h: int = height, w: int = width) -> np.ndarray:
     """
-    Inference an image with trt engine, outputs the resized image and water mask.
+    Load the rgb image from path, then process it as numpy array, as input to trt engine.
     """
+    assert os.path.exists(img_path), f'{img_path} does not exist!'
+
+    # Read image
+    img = Image.open(img_path)
+    # print(f'{img.size=}')
+
+    # Resize image
+    img = img.resize(size=(w, h), resample=Image.Resampling.NEAREST)
+    # print(f'{img.size=}')
+
+    # Convert to numpy array
+    img_arr = np.array(img)
+    # print(f'{img_arr.shape=}')
+
+    # Transpose to (1, C, H, W) and normalize to range [0, 1]
+    img_arr = img_arr.transpose((2, 0, 1))[None].astype(np.float32) / 255
+    # print(f'{img_arr.shape=}')
+
+    return img_arr
+
+
+def post_process_mask(
+        trt_output: List[float],
+        return_tensor: bool = False,
+        h: int = height,
+        w: int = width,
+) -> np.ndarray | torch.Tensor:
+    """
+    Post-process trt inferenced mask to the desired shape and data type.
+    """
+    # Resize
+    pred_mask = np.array(trt_output).reshape(h, w)
+    # print(f'{type(pred_mask)=} {pred_mask.shape=} {pred_mask.dtype=}')
+
+    # Convert to torch, convert to probability, threshold it, change dtype to uint8
+    # pred_mask_torch = torch.from_numpy(pred_mask)
+    pred_mask_torch = torch.tensor(pred_mask, dtype=torch.float32)
+    pred_mask_torch = pred_mask_torch.sigmoid()
+    pred_mask_torch = ((pred_mask_torch > 0.5).float() * 255).to(torch.uint8)
+
+    if return_tensor:
+        return pred_mask_torch
+    else:
+        return pred_mask_torch.detach().cpu().numpy()
+
+
+def get_engine_context(engine_path: str):
     assert os.path.exists(engine_path), f'{engine_path} does not exist!'
 
     TRT_LOGGER = trt.Logger(trt.Logger.INFO)
@@ -154,48 +211,52 @@ def infer(engine_path: str, img_path: str, mask_path: Optional[str] = None) -> T
 
     context = engine.create_execution_context()
 
+    return engine, context
+
+
+def infer(
+        engine_path: str,
+        img_path: str | np.ndarray,
+        mask_path: Optional[str] = None,
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Inference an image with trt engine, outputs the resized image and water mask.
+    """
+    # Get trt engine and context
+    engine, context = get_engine_context(engine_path=engine_path)
+
     # Load image, resize to desired shape, then convert to numpy array
-    img_arr = np.array(Image.open(img_path).resize((width, height)))
-    print(f'After resize: {img_arr.shape=}')
+    if isinstance(img_path, str):
+        img_arr = load_and_process_img(img_path)
+    else:  # Assume the image_arr is of shape (C, H, W) and of uint8 type
+        img_arr = img_path
+        if len(img_arr.shape) < 4 and img_arr.dtype == np.float32 and img_arr.max() <= 1:
+            img_arr = img_arr[None]
+        else:
+            raise ValueError(f'Input image numpy array needs to be from FluvialDataset class (CHW, normalized), '
+                             f'given image shape: {img_arr.shape}, dtype: {img_arr.dtype}.')
 
-    # Transpose numpy array to (C, H, W), increase the 1st dim, then normalize to range [0, 1]
-    img_arr_input = img_arr.transpose((2, 0, 1))[None].astype(np.float32) / 255
-    print(f'After transpose and normalization: {img_arr_input.shape=}')
+    context.set_binding_shape(0, img_arr.shape)
 
-    context.set_binding_shape(0, img_arr_input.shape)
-
-    # s = context.get_binding_shape(0)
-    # print(f'Binding input shape {s}.')
+    trt_start_time = time.time()
 
     # Do trt inference
-    trt_start_time = time.time()
-    trt_outputs = trt_inference(engine, context, img_arr_input)
-    print(f'{trt_outputs=}')
+    trt_outputs = trt_inference(engine, context, img_arr)
+    # print(f'{type(trt_outputs)=} {len(trt_outputs)=} {type(trt_outputs[1])=}')
 
-    # Reshape to get inferred mask
-    pred_mask = np.array(trt_outputs[1]).reshape(height, width)
-    print(f'{pred_mask.shape=}')
-
-    # Use pytorch's sigmoid function to convert to prob, then convert to uint mask
-    pred_mask_torch = torch.from_numpy(pred_mask)
-    pred_mask_torch = pred_mask_torch.sigmoid()
-    pred_mask_torch = ((pred_mask_torch > 0.5).float() * 255).to(torch.uint8)
-    print(f'{pred_mask_torch.shape=}')
+    # Do post-processing to get water mask
+    pred_mask_np = post_process_mask(trt_outputs[1], return_tensor=False)
 
     trt_end_time = time.time()
     print('Inference finished. Time cost: ', trt_end_time - trt_start_time)
 
-    # Convert back to numpy for image display or save
-    pred_mask_output = pred_mask_torch.detach().cpu().numpy()
-    print(f'{pred_mask_output.shape=}')
-
     # Save mask to image
     if mask_path is not None:
-        pred_mask_np_hwc = np.repeat(pred_mask_output[:, :, np.newaxis], 3, axis=2)
+        pred_mask_np_hwc = np.repeat(pred_mask_np[:, :, np.newaxis], 3, axis=2)
         mask = Image.fromarray(pred_mask_np_hwc)
         mask.save(mask_path)
 
-    return img_arr_input, pred_mask_output
+    return img_arr, pred_mask_np
 
 
 def validate_onnx(onnx_model_path: str):
